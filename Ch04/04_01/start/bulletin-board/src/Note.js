fe62fbd749b535fded84c37c52ba796126cf0794
Yi <tangy206@newschool.edu>
2018-10-18 16:34:14 -0400

    20181018 02

--- Ch04/04_01/start/bulletin-board/src/Note.js
+++ Ch04/04_01/start/bulletin-board/src/Note.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, {Component} from 'react'
+import FaPencil from 'react-icons/lib/fa/pencil'
+import FaTrash from 'react-icons/lib/fa/trash'
 
-class Note extends Componenet {
-  render(){
-    return(
-      <div className="note">
-        <p>Learn React</p>
-        <span>
-          <button>Edit</button>
-          <button>Remove</button>
-        </span>
-      <div/>
-    )
-  }
+class Note extends Component {
+    render(){
+        return (
+        <div className="note">
+            <p>Learn React</p>
+            <span>
+                <button>
+                    Edit
+                </button>
+                <button>
+                    Trash
+                </button>
+            </span>
+        </div>
+        )
+    }
 }
-eslint
+
 export default Note
