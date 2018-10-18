59a2118c95b8ffc17d32da9f6f89641f768c6fde
Yi <tangy206@newschool.edu>
2018-10-18 14:53:02 -0400

    First Push

---
+++ Ch04/04_01/start/bulletin-board/src/Note.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Note extends Componenet {
+  render(){
+    return(
+      <div className="note">
+        <p>Learn React</p>
+        <span>
+          <button>Edit</button>
+          <button>Remove</button>
+        </span>
+      <div/>
+    )
+  }
+}
+eslint
+export default Note
